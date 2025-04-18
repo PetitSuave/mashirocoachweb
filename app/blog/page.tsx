62243bd25b40f871ffccacc06d8efb06113de183
PetitSuave <xsimov01@gmail.com>
2025-04-18 16:52:21 +0200

    Added: blogy uz filtruji cihlicky

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -13,71 +13,22 @@ export const metadata = {
 
 export default function BlogPosts() {
     const allBlogs = getBlogPosts(); // Předpokládáme, že tato funkce vrací všechny blogy
-    const filteredBlogs = allBlogs;
 
     return (
-      <div className="relative min-h-screen">
-          <ScreenWideSection>
-              <section className="relative w-screen -ml-[calc(50vw-50%)] bg-teal-800 mb-6">
-                  <div className="flex flex-col w-full max-w-[960px] mx-auto justify-center">
-                      <p className="text-3xl leading-tight font font-semibold mt-16 mb-16 mx-9 text-white">
-                          Blog: Místo, kde se dozvíš víc.
-                      </p>
-                  </div>
-              </section>
-          </ScreenWideSection>
-          <section>
-              <div className="flex flex-wrap">
-                  {/* Předáme všechny blogy do klientské komponenty */}
-                  <BlogFilterComponent allBlogs={allBlogs} />
-
-                  {/* Zobrazení filtrovaných blogů */}
-                  {filteredBlogs
-                      .sort((a, b) => {
-                          if (
-                              new Date(a.metadata.publishedAt) >
-                              new Date(b.metadata.publishedAt)
-                          ) {
-                              return -1;
-                          }
-                          return 1;
-                      })
-                      .map((post) => (
-
-                          <div className="w-full md:w-1/3 mb-6">
-
-                              <div
-                                  className="relative min-h-full border-solid border-teal-600 bg-teal-100 border-2 m-2">
-                                  <Link
-                                      key={post.slug}
-                                      className="flex flex-col space-y-1 transition-opacity duration-200 hover:opacity-80"
-                                      href={`/blog/${post.slug}`}
-                                  >
-                                      <Image src="/photos/joga_temp.jpg" alt={metaData.name}
-                                             width={672} height={672}
-                                             className="sm:max-w-96 mx-auto md:max-w-full"
-                                      />
-
-                                      <div className="p-6">
-                                          <p className="text-black dark:text-white tracking-tight text-sm font-mono mb-3">
-                                              {post.metadata.tags}
-                                          </p>
-                                          <p className="text-black dark:text-white tracking-tight text-xl font-bold mb-3">
-                                              {post.metadata.title}
-                                          </p>
-                                          <p className="text-black dark:text-white tracking-tight">
-                                              {post.metadata.summary}
-                                          </p>
-                                          {/*   <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
-                  {formatDate(post.metadata.publishedAt, false)}
-                </p>*/}
-                                      </div>
-                                  </Link>
-                              </div>
-                          </div>
-                      ))}
-              </div>
-          </section>
-          </div>
-          );
+        <div className="relative min-h-screen">
+            <ScreenWideSection>
+                <section className="relative w-screen -ml-[calc(50vw-50%)] bg-teal-800 mb-6">
+                    <div className="flex flex-col w-full max-w-[960px] mx-auto justify-center">
+                        <p className="text-3xl leading-tight font font-semibold mt-16 mb-16 mx-9 text-white">
+                            Blog: Místo, kde se dozvíš víc.
+                        </p>
+                    </div>
+                </section>
+            </ScreenWideSection>
+            <section>
+                {/* Předáme všechny blogy do klientské komponenty */}
+                <BlogFilterComponent allBlogs={allBlogs}/>
+            </section>
+        </div>
+);
           }
--- app/components/blogFilter.tsx
+++ app/components/blogFilter.tsx
@@ -1,6 +1,9 @@
 'use client'; // Tento řádek je potřeba, aby byla komponenta považována za klientskou
 
 import React, { useState } from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import {metaData} from "../config";
 
 export function BlogFilterComponent({ allBlogs }: { allBlogs: any[] }) {
     const [selectedTag, setSelectedTag] = useState('all'); // Stav pro výběr tagu
@@ -27,14 +30,53 @@ export function BlogFilterComponent({ allBlogs }: { allBlogs: any[] }) {
                 <option value="koučink">Koučink</option>
             </select>
 
-            <div>
-                <h2>Filtrované blogy:</h2>
-                {filteredBlogs.map((post) => (
-                    <div key={post.slug}>
-                        <h3>{post.metadata.title}</h3>
-                        <p>Tag: {post.metadata.tags}</p>
-                    </div>
-                ))}
+            <div className="flex flex-wrap">
+
+                {/* Zobrazení filtrovaných blogů */}
+                {filteredBlogs
+                    .sort((a, b) => {
+                        if (
+                            new Date(a.metadata.publishedAt) >
+                            new Date(b.metadata.publishedAt)
+                        ) {
+                            return -1;
+                        }
+                        return 1;
+                    })
+                    .map((post) => (
+
+                        <div className="w-full md:w-1/3 mb-6">
+
+                            <div
+                                className="relative min-h-full border-solid border-teal-600 bg-teal-100 border-2 m-2">
+                                <Link
+                                    key={post.slug}
+                                    className="flex flex-col space-y-1 transition-opacity duration-200 hover:opacity-80"
+                                    href={`/blog/${post.slug}`}
+                                >
+                                    <Image src="/photos/joga_temp.jpg" alt={metaData.name}
+                                           width={672} height={672}
+                                           className="sm:max-w-96 mx-auto md:max-w-full"
+                                    />
+
+                                    <div className="p-6">
+                                        <p className="text-black dark:text-white tracking-tight text-sm font-mono mb-3">
+                                            {post.metadata.tags}
+                                        </p>
+                                        <p className="text-black dark:text-white tracking-tight text-xl font-bold mb-3">
+                                            {post.metadata.title}
+                                        </p>
+                                        <p className="text-black dark:text-white tracking-tight">
+                                            {post.metadata.summary}
+                                        </p>
+                                        {/*   <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
+                  {formatDate(post.metadata.publishedAt, false)}
+                </p>*/}
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    ))}
             </div>
         </div>
     );
