96421d8bd5876e82a8abea5f08be0cb2e04ae86a
PetitSuave <xsimov01@gmail.com>
2025-04-18 14:41:58 +0200

    Added: Motto on blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
-import { formatDate, getBlogPosts } from "app/lib/posts";
+import {getBlogPosts } from "app/lib/posts";
 import {metaData} from "../config";
 import Image from "next/image";
 import React from "react";
+import {ScreenWideSection} from "../components/screen-wide-section";
 
 export const metadata = {
   title: "Blog",
@@ -13,53 +14,64 @@ export default function BlogPosts() {
   let allBlogs = getBlogPosts();
 
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">Blog</h1>
-      <div className="flex flex-wrap">
-        {allBlogs
-          .sort((a, b) => {
-            if (
-              new Date(a.metadata.publishedAt) >
-              new Date(b.metadata.publishedAt)
-            ) {
-              return -1;
-            }
-            return 1;
-          })
-          .map((post) => (
-
-            <div className="w-full md:w-1/3 mb-6">
-
-                <div className="relative min-h-full border-solid border-teal-600 bg-teal-100 border-2 m-2">
-                <Link
-                    key={post.slug}
-                    className="flex flex-col space-y-1 transition-opacity duration-200 hover:opacity-80"
-                    href={`/blog/${post.slug}`}
-                >
-                <Image src="/photos/joga_temp.jpg" alt={metaData.name}
-                       width={672} height={672}
-                       className="sm:max-w-96 mx-auto md:max-w-full"
-                />
-
-                  <div className="p-6">
-                      <p className="text-black dark:text-white tracking-tight text-sm font-mono mb-3">
-                          {post.metadata.tags}
+      <div className="relative min-h-screen">
+          <ScreenWideSection>
+              <section className="relative w-screen -ml-[calc(50vw-50%)] bg-teal-800 mb-6">
+                  <div className="flex flex-col w-full max-w-[960px] mx-auto justify-center">
+                      <p className="text-3xl leading-tight font font-semibold mt-16 mb-16 mx-9 text-white">
+                          Blog: Místo, kde se dozvíš víc.
                       </p>
-                      <p className="text-black dark:text-white tracking-tight text-xl font-bold mb-3">
-                          {post.metadata.title}
-                      </p>
-                      <p className="text-black dark:text-white tracking-tight">
-                          {post.metadata.summary}
-                      </p>
-                      {/*   <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
+                  </div>
+              </section>
+          </ScreenWideSection>
+          <section>
+              <div className="flex flex-wrap">
+                  {allBlogs
+                      .sort((a, b) => {
+                          if (
+                              new Date(a.metadata.publishedAt) >
+                              new Date(b.metadata.publishedAt)
+                          ) {
+                              return -1;
+                          }
+                          return 1;
+                      })
+                      .map((post) => (
+
+                          <div className="w-full md:w-1/3 mb-6">
+
+                              <div
+                                  className="relative min-h-full border-solid border-teal-600 bg-teal-100 border-2 m-2">
+                                  <Link
+                                      key={post.slug}
+                                      className="flex flex-col space-y-1 transition-opacity duration-200 hover:opacity-80"
+                                      href={`/blog/${post.slug}`}
+                                  >
+                                      <Image src="/photos/joga_temp.jpg" alt={metaData.name}
+                                             width={672} height={672}
+                                             className="sm:max-w-96 mx-auto md:max-w-full"
+                                      />
+
+                                      <div className="p-6">
+                                          <p className="text-black dark:text-white tracking-tight text-sm font-mono mb-3">
+                                              {post.metadata.tags}
+                                          </p>
+                                          <p className="text-black dark:text-white tracking-tight text-xl font-bold mb-3">
+                                              {post.metadata.title}
+                                          </p>
+                                          <p className="text-black dark:text-white tracking-tight">
+                                              {post.metadata.summary}
+                                          </p>
+                                          {/*   <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
                   {formatDate(post.metadata.publishedAt, false)}
                 </p>*/}
-                  </div>
-              </Link>
-                </div>
-            </div>
-          ))}
-      </div>
-    </section>
-  );
-}
+                                      </div>
+                                  </Link>
+                              </div>
+                          </div>
+                      ))}
+              </div>
+          </section>
+          </div>
+          );
+          }
