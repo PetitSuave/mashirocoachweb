bbf98f6d84e5f44fb60cfa132151e4f6b70ab5e1
PetitSuave <xsimov01@gmail.com>
2025-04-18 22:01:11 +0200

    Blog page (#4)
    * Changed: blog paage layout init
    
    * Changed: different images can be used and are laoded from mdx. And are shown same size on blog page
    
    * Rebase conflict fix
    
    ---------
    
    Co-authored-by: janurban <jan.urbn@gmail.com>

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { notFound } from "next/navigation";
 import { CustomMDX } from "app/components/mdx";
 import { formatDate, getBlogPosts } from "app/lib/posts";
 import { metaData } from "app/config";
+import Image from "next/image";
+import React from "react";
 
 export async function generateStaticParams() {
   let posts = getBlogPosts();
@@ -62,40 +64,68 @@ export default function Blog({ params }) {
   }
 
   return (
-    <section>
-      <script
-        type="application/ld+json"
-        suppressHydrationWarning
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "BlogPosting",
-            headline: post.metadata.title,
-            datePublished: post.metadata.publishedAt,
-            dateModified: post.metadata.publishedAt,
-            description: post.metadata.summary,
-            image: post.metadata.image
-              ? `${metaData.baseUrl}${post.metadata.image}`
-              : `/og?title=${encodeURIComponent(post.metadata.title)}`,
-            url: `${metaData.baseUrl}/blog/${post.slug}`,
-            author: {
-              "@type": "Person",
-              name: metaData.name,
-            },
-          }),
-        }}
-      />
-      <h1 className="title mb-3 font-medium text-2xl tracking-tight">
-        {post.metadata.title}
-      </h1>
-      <div className="flex justify-between items-center mt-2 mb-8 text-medium">
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          {formatDate(post.metadata.publishedAt)}
-        </p>
+      <div>
+        <script
+            type="application/ld+json"
+            suppressHydrationWarning
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "BlogPosting",
+                headline: post.metadata.title,
+                datePublished: post.metadata.publishedAt,
+                dateModified: post.metadata.publishedAt,
+                description: post.metadata.summary,
+                image: post.metadata.image
+                    ? `${metaData.baseUrl}${post.metadata.image}`
+                    : `/og?title=${encodeURIComponent(post.metadata.title)}`,
+                url: `${metaData.baseUrl}/blog/${post.slug}`,
+                author: {
+                  "@type": "Person",
+                  name: metaData.name,
+                },
+              }),
+            }}
+        />
+        <section className="relative bg-amber-100">
+          <div className="flex mt-12 mb-12 mx-auto flex-col-reverse md:flex-row ">
+            <div className="flex md:flex-row md:w-1/3 flex-col md:ml-9 mb-9 ">
+              <div className="prose prose-neutral dark:prose-invert">
+                <h1 className="mb-12 text-2xl font-medium tracking-tight">
+                  {post.metadata.title}
+                </h1>
+                <p>
+                  {post.metadata.summary}
+                </p>
+              </div>
+            </div>
+            <div className="md:w-2/3 m-6">
+              {post.metadata.image && (
+                  <Image
+                      src={post.metadata.image}
+                      alt={post.metadata.title}
+                      width={1280}
+                      height={720}
+                      className="object-cover w-full h-auto"
+                  />
+              )}
+            </div>
+          </div>
+        </section>
+        <section className="">
+        <h1 className="title mb-3 font-medium text-4xl tracking-tight">
+          {post.metadata.title}
+        </h1>
+        <div className="flex justify-between items-center mt-2 mb-8 text-medium">
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+            {formatDate(post.metadata.publishedAt)}
+          </p>
+        </div>
+        <article className="prose prose-quoteless prose-neutral dark:prose-invert">
+          <CustomMDX source={post.content}/>
+        </article>
+        </section>
       </div>
-      <article className="prose prose-quoteless prose-neutral dark:prose-invert">
-        <CustomMDX source={post.content} />
-      </article>
-    </section>
+
   );
 }
